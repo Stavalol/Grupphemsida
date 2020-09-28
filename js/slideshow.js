70e0837829a8f66c336cc8222d5ee2b2e0c6a6ff
TimmieO <timmieoskarsson01@gmail.com>
2020-09-28 13:03:09 +0200

    sda

---
+++ js/slideshow.js
@@ -0,0 +1,21 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+var para = document.querySelector('#welcomebox');
+var compStyles = window.getComputedStyle(para);
+
+var marginRight = compStyles.getPropertyValue('margin-right');
+
+document.getElementById("buttons").style.marginRight = marginRight;
